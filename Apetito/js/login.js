1a58f5438a5f057a108baea66cbf59901c27ca4a
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2023-09-22 15:21:29 -0500

    asad

--- Apetito/js/login.js
+++ Apetito/js/login.js
@@ -1,5 +1,5 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpButton = document.getElementById('regist');
+const signInButton = document.getElementById('iniciar');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
