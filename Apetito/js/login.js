e61d7ea70cdc141f69494340816141d85f300acd
Richard Buendia Cordova <buendiacordovarichard@gmail.com>
2023-09-23 00:00:37 -0500

    nada

--- Apetito/js/login.js
+++ Apetito/js/login.js
@@ -3,9 +3,9 @@ const iniciarBtn = document.getElementById('iniciar');
 const container = document.getElementById('container');
 
 registrarseBtn.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+	container.classList.add("panel-activo");
 });
 
 iniciarBtn.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+	container.classList.remove("panel-activo");
 });
