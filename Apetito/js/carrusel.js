54c5baaa72efa89543e486df68b50aeb866e7bb9
ValeriyCapcha <valeriycapcha08@gmail.com>
2023-10-14 01:03:50 -0500

    CARRUSEL 2, la amenaza

--- Apetito/js/carrusel.js
+++ Apetito/js/carrusel.js
@@ -1,41 +1,95 @@
+/* Variables */
 const btnLeft = document.querySelector(".btn-left"),
       btnRight = document.querySelector(".btn-right"),
       slider = document.querySelector("#slider"),
       sliderSection = document.querySelectorAll(".slider-section");
 
+let operacion = 0
+let i = 0
+let mover = 100 / sliderSection.length;
 
+slider.style.transition = "all ease .6s"
+
+/* EVENTOS */
 btnLeft.addEventListener("click", e => moveToLeft())
 btnRight.addEventListener("click", e => moveToRight())
 
-let operacion = 0
-let i = 0
-let widthImg = 100 / sliderSection.length;
+/*Se Mueve cada X segundos*/
+setInterval(() => {
+    moveToRight()
+}, 5000);
 
+/* Funciones */
+console.log(sliderSection.length);
 function moveToRight() {
-    if (i >= sliderSection.length-1) {
+    if (i >= sliderSection.length-3) {
         i = 0;
         operacion = 0;
         slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "none";
         return;
     } 
     i++;
-    operacion = operacion + widthImg;
+    operacion = operacion + mover;
     slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .6s"
     
 }  
 
 function moveToLeft() {
     i--;
     if (i < 0 ) {
-        i = sliderSection.length-1;
-        operacion = widthImg * (sliderSection.length-1)
+        operacion = mover * (sliderSection.length-3)
         slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "none";
+        i = sliderSection.length-3;
+        return;
+    }else if(i == 0){
+        i--;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        return;
+        /*Por algún motivo si i vale 0 sin esto la operacion explota*/
+    }
+    operacion = operacion - mover;
+    slider.style.transform = `translate(-${operacion}%)`;
+}
+ 
+/*
+aplica para el responsive
+function moveToRight() {
+    if (i >= sliderSection.length-1) {
+        i = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        console.log(i);
+        console.log(operacion);
         return;
     } 
-    operacion = operacion - widthImg;
+    i++;
+    operacion = operacion + mover;
     slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .6s"
-}   
+    console.log(i);
+    console.log(operacion);
+    
+}  
+
+function moveToLeft() {
+    if (i <= 0 ) {
+        i = sliderSection.length-1;
+        operacion = mover * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        console.log(i);
+        console.log(operacion);
+        return;
+    }else if(i == 0){
+        i--;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        console.log(i);
+        console.log(operacion);
+        return;
+    }
+    i--;
+    operacion = operacion - mover;
+    slider.style.transform = `translate(-${operacion}%)`;
+    console.log(i);
+    console.log(operacion);
+} */
