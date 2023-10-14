0823536b770839abf8f3519d99169df1bee54789
ValeriyCapcha <valeriycapcha08@gmail.com>
2023-10-14 14:31:42 -0500

    login mensaje error

--- Apetito/js/login.js
+++ Apetito/js/login.js
@@ -9,3 +9,12 @@ registrarseBtn.addEventListener('click', () => {
 iniciarBtn.addEventListener('click', () => {
 	container.classList.remove("panel-activo");
 });
+function mostrarMensaje() {
+	var mensaje = document.getElementById("mensaje");
+
+	mensaje.style.display = 'flex';
+
+	setTimeout(function () {
+		mensaje.style.display = 'none';
+	}, 2500);
+}
